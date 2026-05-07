40684f1421066aa296a6e3af5c30c2de74e3f005
Prakhar Kaushik <105727196+Shadowwfall@users.noreply.github.com>
2026-05-07 13:44:50 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+        freq = [0]*26
+        for char in s:
+            freq[ord(char) - ord('a')] += 1
+        for char in t:
+            freq[ord(char) - ord('a')] -= 1
+        for i in freq:
+            if i != 0: return False
+        return True
