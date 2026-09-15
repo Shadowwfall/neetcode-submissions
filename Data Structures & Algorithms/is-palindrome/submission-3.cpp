3ebabcd37a0c9c322f729de32db0dd7ef00ee87f
Prakhar Kaushik <105727196+Shadowwfall@users.noreply.github.com>
2026-09-15 23:05:54 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+        while(l <= r){
+            while((l < r) && (!isalnum(s[l]))){
+                l++;
+            }
+            while((l < r) && (!isalnum(s[r]))){
+                r--;
+            }
+            if(tolower(static_cast<unsigned char>(s[l])) != tolower(static_cast<unsigned char>(s[r]))){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
